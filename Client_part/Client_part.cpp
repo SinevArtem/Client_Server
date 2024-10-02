210168fced23261a981548671a49c0bee8ace011
SinevArtem <aasinev@inbox.ru>
2024-10-02 21:04:33 +0300

    first commit

---
+++ Client_part/Client_part.cpp
@@ -0,0 +1,64 @@
+﻿#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+SOCKET Connection;
+
+void ClientHandler() 
+{
+	char msg[256];
+	while (true)
+	{
+		recv(Connection, msg, sizeof(msg), NULL); // получение сообщения от сервера
+		cout << msg << endl;
+	}
+}
+
+int main()
+{
+
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1); 
+	if (WSAStartup(DLLVersion, &wsaData) != 0) 
+	{
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(8080);
+	addr.sin_family = AF_INET;
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL); // сокет для соединения с сервером
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) // подсоединение к серверу
+	{
+		cout << "Error: No connect whith server" << endl;
+		return 1;
+	}
+	cout << "Connected" << endl;
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+
+	char msg2[256];
+	while (true)
+	{
+		cin.getline(msg2, sizeof(msg2));
+		send(Connection, msg2, sizeof(msg2), NULL);
+		Sleep(10);
+	}
+
+	system("pause");
+
+	return 0;
+
+}
---
+++ Server_part/Server_part.cpp
@@ -0,0 +1,77 @@
+﻿
+#include <WS2tcpip.h>
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+SOCKET Connections[100];
+int Counter = 0;
+
+void ClientHandler(int index) {
+	char msg[256];
+	while (true)
+	{
+		recv(Connections[index], msg, sizeof(msg), NULL);
+		for (int i = 0; i < Counter; i++) // отправка сообщения всем пользователям
+		{
+			if (index == i) continue;
+			send(Connections[i], msg, sizeof(msg), NULL);
+		}
+	}
+}
+
+
+int main()
+{
+
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1); // версия WinSock2.h
+	if (WSAStartup(DLLVersion, &wsaData) != 0) // загрузка библиотеки и проверка загрузилась ли
+	{
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	// заполнение информации об адресе сокета
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(8080);
+	addr.sin_family = AF_INET; // для интернет протоколов
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // СОЗДАНИЕ СОКЕТА
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // привязка адреса к сокету
+	listen(sListen, SOMAXCONN); // прослушивание порта в ожидании клиента, SOMAXCONN- макс доп число запросов(можем поставить число например 3)
+
+	// новый сокет для удержания подключения с клиентом
+	SOCKET newConnection;
+	for (int i = 0; i < 100; i++)
+	{
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+		if (newConnection == 0) //проверка подключился ли клиент к серверу
+		{
+			cout << "Error 2" << endl;
+		}
+		else
+		{
+			cout << "Client connected" << endl;
+			char msg[256] = "hello";
+			send(newConnection, msg, sizeof(msg), NULL); // отправка сообщения
+		}
+		Connections[i] = newConnection;
+		Counter++;
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(i), NULL, NULL); // создание нового потока
+
+	}
+
+
+	system("pause");
+	return 0;
+}
+
